d52b4fcf9c08e8b0cc4de8a35a9da316caf66cf3
BiMalxMe <journeyxbimal@gmail.com>
2025-04-25 23:11:01 +0545

    SomeIteration

---
+++ src/Iterators.rs
@@ -0,0 +1,42 @@
+
+//______________________Understanding the Iterators____________________
+
+//under the hood uses the --------------------->into_iter()
+// fn main(){
+//     let vecs = vec![1,2,3,4];
+//     //ownership is transferred to i
+//      for i in vecss{
+//         println!("Got {} ",i);
+//      };
+
+//      //It cant print as the valueis already moved
+//      println!("{:?}",vecs);
+// }
+
+//-----------------------iter()---------------------------------------------
+// but using iter() function we can pass the referecnce only
+// fn main(){
+//     let vecs = vec![1,2,3,4];
+//     //ownership is not transferrred as reference is passed only to i
+//      for i in vecs.iter(){
+//         println!("Got {} ",i);
+//      };
+
+//      //It can print as it is owner
+//      println!("{:?}",vecs);
+// }
+
+//-----------------------iter.mut()---------------------------------------------
+fn main(){
+    let mut vecs  = vec![1,2,4];
+    //iterating over the mutable reference
+    let final_vec = vecs.iter_mut();
+    for i in final_vec{
+        //changin the value as mutable refernce is passed
+        //point to the original value
+        *i = *i*10;
+        println!("The values is {}",i);
+        
+    }
+    println!("The original vector is now changed into {:?}",vecs)
+}
