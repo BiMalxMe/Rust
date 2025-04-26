0dfee3b58c2fb3878b0127f8156b81b2553af344
BiMalxMe <journeyxbimal@gmail.com>
2025-04-26 08:31:40 +0545

    IteratorsCompleted

--- src/Iterators.rs
+++ src/Iterators.rs
@@ -2,32 +2,32 @@
 //______________________Understanding the Iterators____________________
 
 //under the hood uses the --------------------->into_iter()
-// fn main(){
-//     let vecs = vec![1,2,3,4];
-//     //ownership is transferred to i
-//      for i in vecss{
-//         println!("Got {} ",i);
-//      };
+fn main(){
+    let vecs = vec![1,2,3,4];
+    //ownership is transferred to i
+     for i in vecss{
+        println!("Got {} ",i);
+     };
 
-//      //It cant print as the valueis already moved
-//      println!("{:?}",vecs);
-// }
+     //It cant print as the valueis already moved
+     println!("{:?}",vecs);
+}
 
 //-----------------------iter()---------------------------------------------
 // but using iter() function we can pass the referecnce only
-// fn main(){
-//     let vecs = vec![1,2,3,4];
-//     //ownership is not transferrred as reference is passed only to i
-//      for i in vecs.iter(){
-//         println!("Got {} ",i);
-//      };
+fn main1(){
+    let vecs = vec![1,2,3,4];
+    //ownership is not transferrred as reference is passed only to i
+     for i in vecs.iter(){
+        println!("Got {} ",i);
+     };
 
-//      //It can print as it is owner
-//      println!("{:?}",vecs);
-// }
+     //It can print as it is owner
+     println!("{:?}",vecs);
+}
 
-//-----------------------iter.mut()---------------------------------------------
-fn main(){
+// //-----------------------iter.mut()---------------------------------------------
+fn main2(){
     let mut vecs  = vec![1,2,4];
     //iterating over the mutable reference
     let final_vec = vecs.iter_mut();
@@ -39,4 +39,20 @@ fn main(){
         
     }
     println!("The original vector is now changed into {:?}",vecs)
+}
+
+
+//----------------------------------.next()---------------------------------------------
+fn main3(){
+    let mut vectr = vec![1,2,3,4,5];
+    //sending the mutable reference such that it can change the original vals
+    let mut  final_vec  = vectr.iter_mut();
+    
+    //another way 
+    while let Some(val) = final_vec.next(){
+        *val += 2;
+        println!("{} ,",val);
+    }
+    //still the vectr has ownership
+    print!("{:?}",vectr)
 }
